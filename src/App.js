c31cbe07e50a060706a8c20fc71850850754a621
Fikri <jamesfikrii@gmail.com>
2019-02-26 14:18:55 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Footer from './components/footer'
+import Navbar from './components/header'
+import Form from './components/form'
+import Homepage from './components/homepage'
+import {Route} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
+  btnClick = () => {
+    alert('Masuk')
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar/>
+        <div className='container'>
+          <Route path='/form' component={Form} />
+          <Route path='/' component={Homepage} exact />
+        </div>
+        <Footer/>
       </div>
+      
     );
   }
 }
---
+++ src/components/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Homepage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Selamat Datang Di Website</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Homepage;
